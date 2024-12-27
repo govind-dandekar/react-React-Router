9626e7891d89d91d42b40c8692045187f630c027
Govind Dandekar <govind.dandekar@gmail.com>
2024-12-26 21:38:00 -0800

    add first route with createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  // define path for which route should be active
+  // path is part after domain
+  {path: '/', element: <HomePage />},
+])
+
+// 1] define routes to support
+// 2] activate router and load route definitions
+// 3] have all components that we cwant to load + enable navigation
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
