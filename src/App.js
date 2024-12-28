016135166d4c3216d0e3f997de97aefdf13ccdb6
Govind Dandekar <govind.dandekar@gmail.com>
2024-12-27 16:34:11 -0800

    add parents and child layouts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { 
+  createBrowserRouter, 
+  //createRoutesFromElements, 
+  RouterProvider,
+  //Route
+   } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import RootLayout from "./pages/Root";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
-  // define path for which route should be active
-  // path is part after domain
-  {path: '/', element: <HomePage />},
+  {
+    // parent route wrapper of child routes
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      // define path for which route should be active
+      // path is part after domain
+      {path: '/', element: <HomePage />},
+      {path: '/products', element: <ProductsPage />}
+    ]
+  },
+  
 ])
 
+// const router = createBrowserRouter(routeDefinitions);
 // 1] define routes to support
 // 2] activate router and load route definitions
-// 3] have all components that we cwant to load + enable navigation
+// 3] have all components that we want to load + enable navigation
 function App() {
   return <RouterProvider router={router}/>;
 }
